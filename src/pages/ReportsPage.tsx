25a47ed6c40e30de48830e58adc729cbff4805f7
controldecalidadrc50602-pixel <controldecalidadrc50602@gmail.com>
2026-02-24 10:34:25 -0600

    Update ReportsPage.tsx

--- src/pages/ReportsPage.tsx
+++ src/pages/ReportsPage.tsx
@@ -4,5 +4,5 @@ import { useApp } from '../context/AppContext';
 
 export const ReportsPage: React.FC = () => {
     const { lang } = useApp();
-    return <Reports lang={lang} />;
+    return <Reports />;
 };
